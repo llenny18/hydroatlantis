1211ffdc353a932707be12469dc64b3d02c3e5a8
llenny18 <ramosalleneid01@gmail.com>
2025-03-07 15:56:39 +0800

    sdfsdfsdfsdgfd f

--- render.py
+++ render.py
@@ -1,3 +1,3 @@
-pip install Django psycopg2-binary djangorestframework python-decouple django-cors-headers djangorestframework-simplejwt cryptography mysqlclient gunicorn whitenoise
+pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
