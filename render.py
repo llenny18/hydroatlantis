5052878a029241ee55920148fb85f07bb9896ccd
llenny18 <ramosalleneid01@gmail.com>
2025-03-07 15:54:57 +0800

    workin na sa local he hasyasasa

--- render.py
+++ render.py
@@ -1,3 +1,3 @@
-pip install Django psycopg2-binary0 djangorestframework python-decouple django-cors-headers djangorestframework-simplejwt cryptography mysqlclient gunicorn whitenoise
+pip install Django psycopg2-binary djangorestframework python-decouple django-cors-headers djangorestframework-simplejwt cryptography mysqlclient gunicorn whitenoise
 python manage.py collectstatic --noinput
 python manage.py migrate
