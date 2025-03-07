61f76b97dd32a81c19eb470107158d55ec03cbef
llenny18 <ramosalleneid01@gmail.com>
2025-03-07 15:58:11 +0800

    tapusin na  a aad

--- render.py
+++ render.py
@@ -1,3 +1,14 @@
-pip install -r requirements.txt
+import subprocess
+
+def install_requirements():
+    try:
+        subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
+        print("Requirements installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+
+if __name__ == "__main__":
+    install_requirements()
+
 python manage.py collectstatic --noinput
 python manage.py migrate
